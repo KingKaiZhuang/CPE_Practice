036133382344e6be134ebc5d3ae49d69962be506
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-01-06 17:19:01 +0800

    Update 10931 Parity.cpp

--- cpe49題/10931 Parity.cpp
+++ cpe49題/10931 Parity.cpp
@@ -23,3 +23,29 @@ int main(){
 		cout << endl;
 	}
 }
+
+// 法二
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	int n;
+	while(cin>>n){
+		if(n==0)break;
+		vector<int>seq;
+		int result = 0;
+		int tmp=n;
+		
+		while(tmp>0){			
+			result += tmp & 1;
+			seq.push_back(tmp & 1);
+			tmp >>= 1;		
+		}
+		cout << "The parity of ";
+		for(int i=(int)seq.size()-1;i>=0;i--){
+			cout << seq[i];
+		}
+		cout << " is " << result << " (mod 2)." << endl;
+	}
+}
