a01c4d16351707c02531e79d5b4a70200ed35771
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-01-10 23:33:59 +0800

    Update 10222 Decode the Mad man.cpp

--- cpe49題/10222 Decode the Mad man.cpp
+++ cpe49題/10222 Decode the Mad man.cpp
@@ -32,3 +32,33 @@ int main() {
     }
 }
 // 簡化版本
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string kb="`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
+
+void revealResult(string s){
+	vector<char>list;
+	int tmp;
+	for(int i=0;i<s.length();i++){
+		char target=tolower(s[i]);
+		tmp=kb.find(target);
+		if(tmp==-1){
+			list.push_back(s[i]);
+			continue;
+		}
+		list.push_back(kb[tmp-2]);
+	}
+	for(int i=0;i<list.size();i++){
+		cout<<list[i];
+	}
+	cout<<endl;
+}
+
+int main() {
+	string s;
+	while(getline(cin,s)){
+		revealResult(s);
+	}
+}
