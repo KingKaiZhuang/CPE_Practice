9cb78c75fb266e06b3634793742adb88e83f57ab
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-01-11 22:26:30 +0800

    Update 10038 Jolly Jumpers.cpp

--- cpe49題/10038 Jolly Jumpers.cpp
+++ cpe49題/10038 Jolly Jumpers.cpp
@@ -38,3 +38,49 @@ int main() {
     	cout<<endl;
     }
 }
+// 整理過後
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void jollyFun(int n){
+	vector<int>list;
+	int tmp;
+	bool isJolly=true;
+	for(int i=0;i<n;i++){
+		cin>>tmp;
+		list.push_back(tmp);
+	}
+	
+	vector<bool>seq(n,false);
+	for(int i=1;i<list.size();i++){
+		int diff=abs(list[i]-list[i-1]);
+		if(diff>0&&diff<=n-1){
+			seq[diff]=true;
+		}else{
+			seq[diff]=false;
+			break;
+		}
+	}
+	
+	for(int i=1;i<n;i++){
+		if(seq[i]==false){
+			isJolly=false;
+			break;
+		}
+	}
+	
+	if(isJolly){
+		cout<<"Jolly"<<endl;
+	}else{
+		cout<<"Not jolly"<<endl;
+	}
+}
+
+int main() {
+	int n;
+	
+	while(cin>>n){
+		jollyFun(n);
+	}
+}
