9a98fd9327b22a21bc235221750d771315d458ea
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-01-14 15:40:53 +0800

    Update 10922 2 the 9s.cpp

--- cpe49題/10922 2 the 9s.cpp
+++ cpe49題/10922 2 the 9s.cpp
@@ -2,34 +2,39 @@
 #include <vector>
 using namespace std;
 
-int main() {
-	string s;
-	while(cin>>s){
-		if(s=="0")break;
-		//cout<<s<<endl;
-		vector<int>nineList;
-		for(int i=0;i<s.length();i++){
-			char tmp=s[i];
-			nineList.push_back(tmp-'0');
-		}
-		
-		int sum=0;
-		for(int i=0;i<nineList.size();i++){
-			sum+=nineList[i];
-		}
-		cout<<sum<<endl;
-		
-		int nineCase=0;
-		while(sum<=9){
-		
-		}
+int result=1;
 
-		
-		/*if((sum%9)==0){
-			while(sum>=9){
-				
-			}
-		}*/
-		cout<<nineCase<<endl;
-	}
+void calculateNum(string s){
+    int sum=0;
+    for(int i=0;i<s.length();i++){
+        sum+=s[i]-'0';
+    }
+    //cout<<sum<<endl;
+    if(sum%9!=0){
+        result=0;
+        //cout<<s<<" is not a multiple of 9."<<endl;
+    }else{
+        if(sum!=9){
+            result++;
+            string tmp=to_string(sum);
+            //cout<<"tmp:"<<tmp<<endl;
+            calculateNum(tmp);
+        }
+    }
+}
+
+int main() {
+    string s;
+    while(cin>>s){
+        if(s=="0") break;
+        result=1;
+        calculateNum(s);
+        
+        if(result == 0){
+            cout << s << " is not a multiple of 9." << endl;
+        } else {
+            cout << s << " is a multiple of 9 and has 9-degree " << result << "." << endl;
+        }     
+    }
+
 }
