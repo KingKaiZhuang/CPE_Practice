80cf5be3dbe56c0ad75f3fcb62dac5d410cc0e00
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-01-14 18:16:00 +0800

    Update 10922 2 the 9s.cpp

--- cpe49題/10922 2 the 9s.cpp
+++ cpe49題/10922 2 the 9s.cpp
@@ -1,40 +1,37 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int result=1;
+int result;
 
-void calculateNum(string s){
-    int sum=0;
-    for(int i=0;i<s.length();i++){
-        sum+=s[i]-'0';
-    }
-    //cout<<sum<<endl;
-    if(sum%9!=0){
-        result=0;
-        //cout<<s<<" is not a multiple of 9."<<endl;
-    }else{
-        if(sum!=9){
-            result++;
-            string tmp=to_string(sum);
-            //cout<<"tmp:"<<tmp<<endl;
-            calculateNum(tmp);
-        }
-    }
+void degreeFun(string s){
+	int tmp1,sum=0;
+	for(int i=0;i<s.length();i++){
+		tmp1=s[i]-'0';
+		sum+=tmp1;
+	}
+	//cout<<"sum:"<<sum<<endl;
+	if(sum%9==0){
+		if(sum>9){
+			result++;
+			s=to_string(sum);
+			degreeFun(s);
+		}
+	}else{
+		result=0;
+	}
 }
 
 int main() {
-    string s;
-    while(cin>>s){
-        if(s=="0") break;
-        result=1;
-        calculateNum(s);
-        
-        if(result == 0){
-            cout << s << " is not a multiple of 9." << endl;
-        } else {
-            cout << s << " is a multiple of 9 and has 9-degree " << result << "." << endl;
-        }     
-    }
-
+	string s;
+	while(cin>>s){
+		result=1;
+		if(s=="0") break;
+		degreeFun(s);
+		if(result!=0){
+			cout<<s<<" is a multiple of 9 and has 9-degree "<<result
+<<"."<<endl;
+		}else{
+			cout<<s<<" is not a multiple of 9."<<endl;
+		}		
+	}
 }
