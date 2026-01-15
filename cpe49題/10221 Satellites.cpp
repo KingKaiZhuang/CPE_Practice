7639fde66fc6471ed98f9ed1acf2a154be32a9c6
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-01-15 22:53:39 +0800

    Update 10221 Satellites.cpp

--- cpe49題/10221 Satellites.cpp
+++ cpe49題/10221 Satellites.cpp
@@ -1,20 +1,19 @@
-#include <iostream>
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main(){
 	double s,a;
-	string min_deg;
-	while(cin>>s>>a>>min_deg){
-		if(min_deg=="min"){
-			a/=60;
-		}
-		if(a>180){
-			a=a-180;
-		}
-		double ang=a*(M_PI/180);
-		double arc=(6440+s)*ang;
-		double chord=(2*((6440+s)*sin(ang/2)));
+	string md;
+	
+	while(cin>>s>>a>>md){
+		if(md=="min")a/=60;
+		if(a>180)a=a-180;
+		
+		double angle=a*(M_PI/180);
+		double arc=angle*(6440+s);
+		double chord=((6440+s)*sin(angle/2))*2;
+		
 		cout<<fixed<<setprecision(6)<<arc<<" "<<chord<<endl;
 	}
 }
