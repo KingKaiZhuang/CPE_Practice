6019c6887c37e4f01a7087dcdf1391d16406ac7c
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-01-17 23:42:01 +0800

    Update 11332 Summing Digits.cpp

--- cpe49題/11332 Summing Digits.cpp
+++ cpe49題/11332 Summing Digits.cpp
@@ -23,3 +23,28 @@ int main(){
 		f(n);
 	}
 }
+// 版本二 遞迴呼叫
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void f(string s){
+		
+	if(s.length()!=1){
+		int r=0;
+		for(int i=0;i<s.length();i++){
+			r+=s[i]-'0';
+		}
+		f(to_string(r));
+	}else{
+		cout<<s<<endl;
+	}
+}
+
+int main(){
+	string s;
+	while(cin>>s){
+		if(s=="0")break;
+		f(s);		
+	}
+}
