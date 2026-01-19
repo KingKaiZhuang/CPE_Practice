aaf1bd010b461b75b0447b977e335f45c417225f
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-01-19 12:34:54 +0800

    Update 10235 Simply Emirp.cpp

--- cpe49題/10235 Simply Emirp.cpp
+++ cpe49題/10235 Simply Emirp.cpp
@@ -65,3 +65,47 @@ int main(){
 
 	}
 }
+# 方法二
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isPrime(int n){
+	if(n==0 || n==1){
+		return false;
+	}
+	for(int i=2;i*i<=n;i++){
+		if(n%i==0){
+			return false;
+		}
+	}
+	return true;
+}
+
+int reverseFun(int n){
+	int r=0;
+	int tmp=0;
+	while(n>0){
+		tmp=n%10;
+		r=r*10+tmp;
+		n/=10;
+	}
+	return r;
+}
+
+int main(){
+	int n;
+	while(cin>>n){
+		int rNum=reverseFun(n);
+		bool Prime=isPrime(n);		
+		bool rPrime=isPrime(rNum);
+		
+		if(!Prime){
+			cout<<n<<" is not prime."<<endl;
+		}else if(Prime&&rPrime&&(rNum!=n)){
+			cout<<n<<" is emirp."<<endl;
+		}else{
+			cout<<n<<" is prime."<<endl;
+		}
+	}
+}
