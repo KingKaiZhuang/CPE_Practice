386ebceea1e6ae3d783120504126104cdea92296
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-02-11 15:19:21 +0800

    Update 10812 Beat the Spread!.cpp

--- cpe49題/10812 Beat the Spread!.cpp
+++ cpe49題/10812 Beat the Spread!.cpp
@@ -1,39 +1,13 @@
-#include <iostream>
+n=int(input())
 
-using namespace std;
-
-int main(){
-	int n;
-	cin >> n;
+for i in range(n):
+	sum,diff=map(int,input().split())
 	
-	while(n--){
-		int val_1,val_2;
-		cin >> val_1 >> val_2;
-		
-		bool found = false;
-
-		for(int i=0;i<val_1;i++){
-			int score_1 = i;
-			int score_2 = val_1 - i;
-			int diff = score_1 - score_2;
-			
-			if((score_1 - score_2) < 0) diff = -(score_1 - score_2);
-			
-			if(diff == val_2){
-				if(score_1 < score_2) {
-					int tmp = score_1;
-					score_1 = score_2;
-					score_2 = tmp;
-				}
-				cout << score_1 << " " << score_2 << endl;
-				found = true;
-				break;
-			}
-		}
-		
-		if(!found) {
-			 cout << "impossible" << endl;
-		}
-	}
-	return 0;
-}
+	if sum<diff or (sum+diff)%2 or (sum-diff)%2:
+		print("impossible")
+		continue
+	
+	x1=(sum+diff)//2
+	x2=(sum-diff)//2
+	
+	print(x1,x2)
