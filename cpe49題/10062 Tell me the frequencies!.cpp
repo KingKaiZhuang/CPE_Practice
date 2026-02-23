0927c9e44049057a53fced83b4c28c95b8635d20
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-02-23 08:55:57 +0800

    Update 10062 Tell me the frequencies!.cpp

--- cpe49題/10062 Tell me the frequencies!.cpp
+++ cpe49題/10062 Tell me the frequencies!.cpp
@@ -1,26 +1,27 @@
-#include <bits/stdc++.h>
+def sort_key(n):
+	return(record[n],-n)
 
-using namespace std;
-
-int main() {
-	string s;
-	bool flag=false;
+isFirst=False
+while True:
+	try:
+		s=input()
+	except EOFError:
+		break
+		
+	if isFirst:
+		print()	
+		
+	record={}
+	num=[]
+	for c in s:
+		if ord(c) in record:
+			record[ord(c)]+=1
+		else:
+			record[ord(c)]=1
+			num.append(ord(c))
+		
+	num.sort(key=sort_key)
 	
-	while(getline(cin,s)){
-		int record[128]={0};
-		for(int i=0;i<s.length();i++){
-			record[(int)s[i]]++;
-		}
-		
-		if(flag)cout<<endl;
-		
-		for(int i=1;i<s.length();i++){
-			for(int j=127;j>=0;j--){
-				if(record[j]==i){
-					cout<<j<<" "<<record[j]<<endl;
-				}
-			}
-		}
-		flag=true;
-	}
-}
+	for i in range(len(num)):
+		print(f"{num[i]} {record[num[i]]}")
+	isFirst=True
