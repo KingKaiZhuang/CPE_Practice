6e0c92706da562765d51205e91716f58a6c9c3bc
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-02-24 21:37:42 +0800

    Update 10922 2 the 9s.py

--- cpe49題/10922 2 the 9s.py
+++ cpe49題/10922 2 the 9s.py
@@ -2,25 +2,25 @@ while True:
 	s=input()
 	if s=="0":
 		break
-	original_s=s
 	
-	sum=0
-	for i in s:
-		sum+=int(i)
+	numList=[]
+	for c in s:
+		numList.append(int(c))
 	
-	if sum%9!=0:
-		print(f"{original_s} is not a multiple of 9.")
+	if sum(numList)%9!=0:
+		print(f"{s} is not a multiple of 9.")
 		continue
-
-	s=str(sum)
+		
+	num=sum(numList)
 	degree=1
-	while True:
-		if s=="9":
-			break		
-		else:
-			tmp=0
-			for i in s:
-				tmp+=int(i)
-			s=str(tmp)
+	if num==9:
+		degree=1
+	else:
+		while num>9:
+			tmp=str(num)
+			total=0
+			for i in tmp:
+				total+=int(i)
+			num=total
 			degree+=1
-	print(f"{original_s} is a multiple of 9 and has 9-degree {degree}.")
+	print(f"{s} is a multiple of 9 and has 9-degree {degree}.")
