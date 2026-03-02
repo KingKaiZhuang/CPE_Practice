ab195c782b3bdfdfa8cc3a15af586f17520ce1ee
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-03-02 10:14:39 +0800

    Update 10642 Can You Solve It?.py

--- cpe49題/10642 Can You Solve It?.py
+++ cpe49題/10642 Can You Solve It?.py
@@ -1,11 +1,15 @@
-def location(x,y):
-	return (x+y)*(x+y+1)/2+x
+def far(x,y):
+	floor=x+y
+	return floor*(floor+1)//2+x
 
 n=int(input())
 caseNum=1
+
 for _ in range(n):
-	x1,y1,x2,y2=input().split()
-	loc1=location(int(x1),int(y1))
-	loc2=location(int(x2),int(y2))
-	print(f"Case {caseNum}: {int(loc2-loc1)}")
+	x1,y1,x2,y2=map(int,input().split())
+	
+	s=far(x1,y1)
+	e=far(x2,y2)
+	
+	print(f"Case {caseNum}: {e-s}")
 	caseNum+=1
