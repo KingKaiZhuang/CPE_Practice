a74544f980c2ca39f2486cb3bf3ad38fcaad9ea6
Zhuang Jun Kai <5b0g0024@stust.edu.tw>
2026-05-21 11:08:27 +0800

    Update 10908 Largest Square.py

--- cpe49題/10908 Largest Square.py
+++ cpe49題/10908 Largest Square.py
@@ -1,42 +1,28 @@
-def isLargest(x,y,grid,add):
-	global h,w
-	
-	top=x-add
-	bottom=x+add
-	left=y-add
-	right=y+add
-	
-	if top<0 or bottom>=h or left<0 or right>=w:
-		return False
-	
-	for i in range(top,bottom+1):
-		for j in range(left,right+1):
-			#print(f"i = {i}, j = {j}")
-			if grid[i][j]!=grid[x][y]:
-				return False
-	return True
+def largest(x,y,rec,r,c,add):
+    top,right,bottom,left=x-add,y+add,x+add,y-add
 
-n=int(input())
+    if top<0 or right>=c or bottom>=r or left<0:
+        return False
+    
+    for i in range(top,bottom+1):
+        for j in range(left,right+1):
+            if rec[i][j]!=rec[x][y]:
+                return False
+    return True
+    
 
-for _ in range(n):
-	h,w,c=map(int,input().split())
-	print(h,w,c)
-	
-	grid=[]
-	
-	for _ in range(h):
-		s=input().strip()
-		grid.append(s)
-	#print(grid)
-	
-	
-	
-	for _ in range(c):
-		x,y=map(int,input().split())
-		#print(x,y)
-		add=0
-		while isLargest(x,y,grid,add):
-			add=add+1
-			
-		print((add-1)*2+1)
-			
+testCase=int(input())
+
+for _ in range(testCase):
+    r,c,t=map(int,input().split())
+    print(r,c,t)
+    rec=[]
+    for _ in range(r):
+        rec.append(input())
+    
+    for _ in range(t):
+        x,y=map(int,input().split())
+        add=0
+        while largest(x,y,rec,r,c,add):
+            add+=1
+        print((add-1)*2+1)
